aea419fe2b38c51dad9c7e025cdd751db8b0e0ca
Daniel Castillo <dlcastillo3015@gmail.com>
2021-12-25 05:24:19 -0500

    La frase cambia en dependencia de la fecha

--- js/app.js
+++ js/app.js
@@ -29,5 +29,29 @@ function modoOscuro() {
     document.body.classList.toggle("general");
 }
 
+// Cambia la frase basándose en la fecha
+function fechasImportantes() {
+    let hoy = new Date();
+    let dia = hoy.getDate();
+    let mes = hoy.getMonth() + 1;
+
+    let mensaje;
+
+    if (mes === 1 && dia === 1) {
+        mensaje = "¡Feliz Año Nuevo!";
+    }
+
+    else if (mes === 12 && dia === 25) {
+        mensaje = "¡Feliz Navidad!";
+    }
+
+    else {
+        mensaje = "El futuro pertenece a los creadores";
+    }
+
+    document.getElementsByTagName("p")[0].textContent = mensaje;
+}
+
 setEdad();
 document.getElementById("switch-label").addEventListener("click", modoOscuro);
+fechasImportantes();
